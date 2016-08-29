d24cf2bd5207f1192afebc83b89b493a95c699e3
Patrick Sanders <patrickhsanders@gmail.com>
2016-08-29 16:13:49 -0400

    removed old routes from root URL file

--- ttt/urls.py
+++ ttt/urls.py
@@ -1,17 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
-# from people.views import StudentList
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.decorators import login_required
 from django.conf.urls import include
 
 from rest_framework import routers
 from people.views import ActiveStudentViewSet
 
 from attendance import urls as attendance_urls
-from people.views import StudentCheckin, StudentList, StudentEmailList, StudentTestEmailList, StudentListPortlet, StudentListStartingSoonPortlet, StudentsCurrentlySignedInPortlet
 from django.views.generic.base import RedirectView
 from dashboard.views import GenericDashboard, AttendanceDashboard
-from people.views import ContactInfoEditView
 from attendance.views import PresentStudentViewSet
 from course.views import CourseViewSet
 from projects.views import ProjectsListView
@@ -21,11 +18,12 @@ from people import urls as people_urls
 from note import urls as note_urls
 from finance import urls as finance_urls
 
+# REST Framework router setup.
 router = routers.DefaultRouter()
 router.register(r'student', ActiveStudentViewSet)
 router.register(r'attendance', PresentStudentViewSet)
 router.register(r'course',CourseViewSet)
-#
+
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/dashboard/')),
     url(r'^admin/', admin.site.urls),
@@ -40,26 +38,10 @@ urlpatterns = [
     url(r'^project/$', login_required(ProjectsListView.as_view())),
     url(r'^place/$', login_required(PlacesChart.as_view())),
 
-    # url(r'^student/list$', login_required(StudentList.as_view())),
-    # url(r'^student/list/portlet$', login_required(StudentListPortlet.as_view())),
-    # url(r'^student/list/starting_soon/portlet$', login_required(StudentListStartingSoonPortlet.as_view())),
-    #
-    # url(r'^student/list/email/$', login_required(StudentEmailList.as_view())),
-    # url(r'^student/list/email/test/$', login_required(StudentTestEmailList.as_view())),
-    #
-    # url(r'^student/portlet/checked-in/$', login_required(StudentsCurrentlySignedInPortlet.as_view()), name="checked_in_portlet"),
-    # url(r'^student/checkin/(?P<success>[\w\-]*)', login_required(StudentCheckin.as_view()), name="checkin"),
-    #
-    #
-    # url(r'^student/(?P<student_id>[\w\-]+)/contact-info/edit', login_required(ContactInfoEditView.as_view()), name="create-contact-info"),
-
-
     url(r'^register/', include(attendance_urls)),
 
     url(r'^accounts/login/$', admin.site.login),
 
     url(r'^dashboard/$', login_required(GenericDashboard.as_view())),
     url(r'^dashboard/attendance/$', login_required(AttendanceDashboard.as_view())),
-
-    # url(r'^api-auth/', include('rest_framework.urls',))
 ]
