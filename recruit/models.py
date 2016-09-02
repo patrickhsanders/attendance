ad353d6a90c1e67e8b37ad3f1c55841ddc95d67c
Patrick Sanders <patrickhsanders@gmail.com>
2016-09-01 22:09:56 -0400

    update to recruit model and migrations

--- recruit/models.py
+++ recruit/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
 
+class Company(models.Model):
+    name = models.CharField(max_length=63)
+    website = models.URLField(blank=True)
+    city = models.CharField(max_length=31, blank=True)
+
+
 class Job(models.Model):
     title = models.CharField(max_length=63)
-    company = models.CharField(max_length=63)
     salary = models.FloatField(blank=True)
     start_date = models.DateField(blank=True)
     end_date = models.DateField(blank=True)
-    city = models.CharField(max_length=31, blank=True)
+    company = models.OneToOneField(Company, blank=True)
