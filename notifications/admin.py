60112d5212a0b7b0a66b12a31c19a4bf0dede12c
Patrick Sanders <patrickhsanders@gmail.com>
2016-10-14 22:50:09 -0400

    Updated model and admin

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import NotificationLog
+
 # Register your models here.
+class NotificationLogAdmin(admin.ModelAdmin):
+    list_display = ('sender','date','subject', 'fake')
+    list_filter = ('recipient','sender','date')
+    search_fields = ('recipient','sender','date')
+
+admin.site.register(NotificationLog, NotificationLogAdmin)
--- notifications/models.py
+++ notifications/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
-from people.models import Student
+# from people.models import Student
 # Create your models here.
 
 
@@ -21,9 +21,13 @@ class UnsubscribeToken(models.Model):
 
 class NotificationLog(models.Model):
 
-    recipient = models.ManyToManyField(Student)
+    recipient = models.ForeignKey('people.Student')
     sender = models.CharField(max_length=31)
     date = models.DateTimeField(auto_now_add=True)
     subject = models.CharField(max_length=127, blank=True, null=True)
     content = models.TextField()
+    fake = models.BooleanField(default=False)
 
+    def __str__(self):
+        recipients = ', '.join(self.recipient)
+        output = "{} to {} from {} on {}".format(self.subject, recipients, self.sender, self.date)
